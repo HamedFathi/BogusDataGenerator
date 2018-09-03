e4ed01726ba5d480bdfda63e112735e1fe1fb344
Hamed Fathi <HamedFathi@users.noreply.github.com>
2018-09-03 15:22:58 +0430

    chore(InnerTypeResult)

--- BogusDataGenerator/InnerTypeResult.cs
+++ BogusDataGenerator/InnerTypeResult.cs
@@ -7,11 +7,8 @@ namespace BogusDataGenerator
         public int Level { get; set; }
         public Type Type { get; set; }
         public TypeStatus Status { get; set; }
-        public string Name => Type.Name;
+        public string Name => Type.ToString();
         public string FullName => Type.FullName;
-        public string UnderlyingSystemTypeName => Type.UnderlyingSystemType.Name;
-        public string UnderlyingSystemTypeFullName => Type.UnderlyingSystemType.FullName;
-
     }
 
 
